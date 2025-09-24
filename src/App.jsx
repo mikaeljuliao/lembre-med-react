da2cc04f0ded28e0f3f614084963890f1c372e87
mikael <mikaeljuliao56@gmail.com>
2025-09-23 22:05:55 -0300

    projeto inicial funcionando

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="bg-red-500 text-white p-6 text-center text-2xl">
+      Tailwind v4 funcionando com fundo azul!
+    </div>
+  );
+}
